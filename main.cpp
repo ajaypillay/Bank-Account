1f314d87d09407af51afac61204b7adaa0053b01
karkin69 <munkarkin123@hotmail.com>
2016-01-14 17:57:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include "main.h"
-#include <iostream>
-using namespace std;
+#include <string>
 
 int main (){
-hihhfnasdj
+string name; 
+	int age, accountNumber; 
+	char gender;
+	char typeDeposit;
+
+	cout << "Please enter your name: ";
+	
+	cin >>name;
+	fflush(stdin);
+	cout <<"Please enter your account number: " ;
+	cin >> accountNumber; 
+	cout <<"Please enter your age: ";
+	cin >> age; 
+	cout <<"Please enter your gender (M/m/F/f): "; 
+	cin >> gender;
+	cout << "Please enter the type of account: " ; 
+	cout << "Input s for Saving Account" << endl << "Input g for Golden Account" << endl << "Input f for Fixed Deposit";
+	cin >> typeDeposit;
+
+	bankAccount account1(name,accountNumber, age, typeDeposit, gender); 
+	balanceCheck account_1;  
+
+	account1.showBankAccount();
+	account_1. inputWithdrawalDeposit();
+	account_1.updateBalance();
+	account_1. showBalance();
+
+	return 0;
 
 } 

c37d48a21755882b8ebecd99d11d8148c42198b4
karkin69 <munkarkin123@hotmail.com>
2016-01-28 17:29:11 +0800

    Update main.h

--- main.h
+++ main.h
@@ -2,9 +2,8 @@
 #include <string> 
 using namespace std; 
 
-#include <iostream>
-#include <string> 
-using namespace std; 
+
+// ajay will take care of the assigning of account number 
 
 class bankAccount {
 protected: // may need to be changed to protected later 
@@ -16,13 +15,34 @@ protected: // may need to be changed to protected later
   //saving account = s ;
   //golden account = g ;
   //fixed deposit = f ;
+  // NIL = n 
   
 public:
   bankAccount (string, int, int, char, char); // initialize private/protected data members
   void showBankAccount (void);
+  void checkGoldenAccountValidity (void); 
+  void reselectAccount (void); 
   
 };
 
+void bankAccount :: reselectAccount (void){
+	cout <<"\nGolden Account is only for users above 55 years old, please reselect your option.";
+	cout <<"\nEnter (s) for Saving Account or (f) for Fixed Deposit: ";
+	cin >> typeDeposit; 
+}
+
+void bankAccount :: checkGoldenAccountValidity (void){
+	if (typeDeposit =='g'){
+	if (age >55 ) {
+		typeDeposit = 'g'; 
+	} else if (age <=55){
+		typeDeposit = 'n';
+		reselectAccount ();  // here 
+	}
+}
+
+
+}
 
   bankAccount :: bankAccount (string n, int acc, int a, char d, char g){
     // remember to use fflush(stdin);
@@ -43,7 +63,6 @@ public:
       cout << "Gender: Female" << endl;
     }
     
-    cout << "Gender: " << gender << endl; 
     cout << "Age: " << age << endl; 
     cout << "This is your account number: " << accountNumber << endl;
     
@@ -65,13 +84,65 @@ public:
     double depositAmount, withdrawalAmount;
     double amountWithdrawal;
     double amountDeposit;
+	double interestSavingAcc;
+	double interestGoldenAcc; 
+	double interestFixedDeposit; 
+	int yearsFixedDeposit; 
   public:
     void inputWithdrawalDeposit(void);
     void updateBalance(void);
     void showBalance(void);
 	balanceCheck(string, int, int, char, char);
+	void accountCheck (void);
   };
+
+  void balanceCheck :: accountCheck (void){
+	  if (typeDeposit == 's'){
+		  while (accountBalance < 100){
+		  cout <<"\nThe minimum balance is $100, please make some deposit!"<<endl;
+		  inputWithdrawalDeposit();
+		  updateBalance();
+		  showBalance(); 
+	  }
+	  interestSavingAcc = 0.25; 
+
+	  } else if (typeDeposit =='g'){
+
+		  if (accountBalance < 50000){
+		  while (accountBalance <100){
+			  cout <<"\nThe minimum balance is $100, please make some deposit!"<<endl;
+			  inputWithdrawalDeposit();
+			  updateBalance();
+			  showBalance();
+		  }
+		  interestGoldenAcc = 0.375; 
+	  }
+
+	  else if (accountBalance > 50000){
+		  interestGoldenAcc = 0.5; 
+		  
+	  } 
+
+	  } else if (typeDeposit =='f'){
+		  while (accountBalance < 5000){
+		  cout <<"\nThe minimum balance is $5000, please make some deposit!"<<endl;
+		  inputWithdrawalDeposit();
+		  updateBalance();
+		  showBalance();
+	  }
+
+	  if (yearsFixedDeposit == 1){ // haven't have a way to assign the yearsFixedDeposit yet, overlap with ajay's work 
+		  interestFixedDeposit = 0.55;
+	  } else if (yearsFixedDeposit == 2){
+		  interestFixedDeposit = 0.7;
+	  } else if (yearsFixedDeposit >= 3){
+		  interestFixedDeposit = 0.8;
+	  }
+	  }
+
+  }
   
+
   void balanceCheck::inputWithdrawalDeposit(void){
     cout <<"Please select the mode (Withdrawal : w / Deposit : d): ";
     cin >> modeBalance; 
@@ -85,9 +156,18 @@ public:
     if (modeBalance == 'w'){
       cout <<"Please enter the amount you wish to withdraw: " << endl;
       cin >> withdrawalAmount;
+	  if (accountBalance < withdrawalAmount){
+		  cout <<"You do not have enough money!"<<endl;
+		  withdrawalAmount = 0;
+	  }
+
     } else if (modeBalance == 'd'){
       cout <<"Please enter the amount you wish to deposit: " << endl;
       cin >> depositAmount;
+	  if (depositAmount < 0){
+		  cout <<"Invalid deposit amount" <<endl;
+		  depositAmount =0;
+	  }
     }
   }
   
@@ -114,9 +194,3 @@ public:
 	withdrawalAmount = 0;
 	accountBalance = 0;
   }
-  
-  /* so what we need to do is to 
-  1. ask if the user wants to withdraw/deposit, and then records the amount ---> inputWithdrawalDeposit();
-  2. then call updateBalance() to update the amount deposited / withdrawn to accountBalance;
-  3. anytime i need to check accountBalance, call showBalance(); 
-  */

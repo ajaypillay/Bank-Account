2a9dbc96ab9b379ee6f4fbe31cf7bad6f322d29d
Ajay Pillay <ajaypillay.sg@gmail.com>
2016-02-13 05:06:03 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,196 +1,356 @@
 #include <iostream>
-#include <string> 
-using namespace std; 
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+#include <limits>
 
+using namespace std;
 
-// ajay will take care of the assigning of account number 
+void clearScreen() {
+    int x = 0;
+    while (x < 25) {
+        cout<<"\n";
+        x++;
+    }
+    return;
+}
 
-class bankAccount {
-protected: // may need to be changed to protected later 
-  string name;
-  int accountNumber;
-  int age;
-  char typeDeposit; 
-  char gender; // m / M / f / F 
-  //saving account = s ;
-  //golden account = g ;
-  //fixed deposit = f ;
-  // NIL = n 
-  
-public:
-  bankAccount (string, int, int, char, char); // initialize private/protected data members
-  void showBankAccount (void);
-  void checkGoldenAccountValidity (void); 
-  void reselectAccount (void); 
-  
+string timeNow() {
+    time_t now = time(0);
+    struct tm  tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+    buf[10] = ' ';
+    return buf;
+}
+
+class Logger {
+    public:
+        void withdrawDeposit(string, string, int, int);
+        void login(string);
+        void logout(string);
+        void newAccount(string, string);
 };
 
-void bankAccount :: reselectAccount (void){
-	cout <<"\nGolden Account is only for users above 55 years old, please reselect your option.";
-	cout <<"\nEnter (s) for Saving Account or (f) for Fixed Deposit: ";
-	cin >> typeDeposit; 
+void Logger::withdrawDeposit(string a, string b, int c, int d){
+    ofstream Log;
+    Log.open("Log.txt", ios_base::app);
+    Log<<"["<<timeNow()<<"] "<<a<<" has "<<b<<" $"<<c<<". Balance: $"<<d<<"\n";
 }
 
-void bankAccount :: checkGoldenAccountValidity (void){
-	if (typeDeposit =='g'){
-	if (age >55 ) {
-		typeDeposit = 'g'; 
-	} else if (age <=55){
-		typeDeposit = 'n';
-		reselectAccount ();  // here 
-	}
+void Logger::login(string a){
+    ofstream Log;
+    Log.open("Log.txt", ios_base::app);
+    Log<<"["<<timeNow()<<"] "<<a<<" has successfully logged in.\n";
 }
 
-
+void Logger::logout(string a){
+    ofstream Log;
+    Log.open("Log.txt", ios_base::app);
+    Log<<"["<<timeNow()<<"] "<<a<<" has successfully logged out.\n";
 }
 
-  bankAccount :: bankAccount (string n, int acc, int a, char d, char g){
+void Logger::newAccount(string a, string b){
+    ofstream Log;
+    Log.open("Log.txt", ios_base::app);
+
+    if (b == "s") {
+        Log<<"["<<timeNow()<<"] "<<a<<" has opened a Saving Account.\n";
+    }
+    else if (b == "d") {
+        Log<<"["<<timeNow()<<"] "<<a<<" has opened a Fixed Deposit Account.\n";
+    }
+    else if (b == "g") {
+        Log<<"["<<timeNow()<<"] "<<a<<" has opened a Golden Account.\n";
+    }
+}
+
+class bankAccount {
+    public:
+        string typeDeposit;
+    protected:
+        string name;
+        int accountNumber;
+        int age;
+        string gender; // m / M / f / F
+        //saving account = s ;
+        //golden account = g ;
+        //fixed deposit = f ;
+        // NIL = n
+    public:
+        bankAccount(string, int, int, string, string); // initialize private/protected data members
+        void showBankAccount(void);
+        void checkGoldenAccountValidity(void);
+        void reselectAccount(void);
+        Logger log;
+};
+
+void bankAccount::reselectAccount(void) {
+    clearScreen();
+    showBankAccount();
+	cout<<"\nThe Golden Account is only for depositors aged 55 years old or above.\nPlease reselect your option."<<endl;
+	cout<<"\nEnter [S] for Saving Account or [F] for Fixed Deposit: ";
+	cin>>typeDeposit;
+	while (typeDeposit != "s" && typeDeposit != "S" && typeDeposit != "f" && typeDeposit != "F"){
+        showBankAccount();
+        cout<<"\nInvalid account type! Please reenter:\n\n";
+        cout<<"\nThe Golden Account is only for depositors aged 55 years old or above.\nPlease reselect your option."<<endl;
+        cout<<"\nEnter [S] for Saving Account or [F] for Fixed Deposit: ";
+        cin>>typeDeposit;
+    }
+}
+
+void bankAccount::checkGoldenAccountValidity(void) {
+	if (typeDeposit == "g") {
+        if (age >= 55) {
+            typeDeposit = "g";
+        }
+        else if (age < 55) {
+            typeDeposit = "n";
+            reselectAccount();  // here
+        }
+    }
+}
+
+bankAccount::bankAccount (string n, int acc, int a, string d, string g) {
     // remember to use fflush(stdin);
     name = n;
     accountNumber = acc;
     age = a;
     typeDeposit = d;
-    gender = g; 
-    
-  }
-  
-  void bankAccount :: showBankAccount (void){
-    cout <<endl<< "Hello " << name << endl; 
-    
-    if (gender == 'm'|| gender == 'M'){
-      cout <<"Gender: Male" << endl; 
-    } else if ( gender == 'f' || gender == 'F'){
-      cout << "Gender: Female" << endl;
+    gender = g;
+}
+
+void bankAccount::showBankAccount(void) {
+    clearScreen();
+    cout<<endl<<"Hello, "<<name<<"!"<<endl<<endl;
+    if (gender == "m" || gender == "M") {
+        cout<<"Gender: Male"<<endl;
     }
-    
-    cout << "Age: " << age << endl; 
-    cout << "This is your account number: " << accountNumber << endl;
-    
-    if ( typeDeposit == 's') {
-      cout << "Type of deposit: Saving Account. " << endl;
-    } else if (typeDeposit == 'g') {
-      cout <<"Type of deposit: Golden Account. " << endl;
-    } else if (typeDeposit == 'f'){
-      cout <<"Type of deposit: Fixed Deposit. " << endl;
-    }
-    
+    else if (gender == "f" || gender == "F") {
+        cout<<"Gender: Female"<<endl;
     }
 
+    cout<<"Age: "<<age<<endl;
+    cout<<"Account Number: GCB-"<<accountNumber<<endl;
 
-  class balanceCheck : public bankAccount { // child of bankAccount
-  private: // not sure if need to be protected yet 
-    double accountBalance;
-    char modeBalance; // d for deposite and w for withdrawal
-    double depositAmount, withdrawalAmount;
-    double amountWithdrawal;
-    double amountDeposit;
-	double interestSavingAcc;
-	double interestGoldenAcc; 
-	double interestFixedDeposit; 
-	int yearsFixedDeposit; 
-  public:
-    void inputWithdrawalDeposit(void);
-    void updateBalance(void);
-    void showBalance(void);
-	balanceCheck(string, int, int, char, char);
-	void accountCheck (void);
-  };
+    if (typeDeposit == "s") {
+        cout<<"Type of account: Saving Account "<<endl;
+    }
+    else if (typeDeposit == "g") {
+        cout<<"Type of account: Golden Account"<<endl;
+    }
+    else if (typeDeposit == "f") {
+        cout<<"Type of account: Fixed Deposit "<<endl;
+    }
+}
 
-  void balanceCheck :: accountCheck (void){
-	  if (typeDeposit == 's'){
-		  while (accountBalance < 100){
-		  cout <<"\nThe minimum balance is $100, please make some deposit!"<<endl;
-		  inputWithdrawalDeposit();
-		  updateBalance();
-		  showBalance(); 
-	  }
-	  interestSavingAcc = 0.25; 
+class balanceCheck:public bankAccount { // child of bankAccount
+    public:
+        double accountBalance;
+    private: // not sure if need to be protected yet
+        string modeBalance; // d for deposite and w for withdrawal
+        double depositAmount, withdrawalAmount;
+        double amountWithdrawal;
+        double amountDeposit;
+        double interestSavingAcc;
+        double interestGoldenAcc;
+        double interestFixedDeposit;
+        int yearsFixedDeposit;
+    public:
+        balanceCheck(string, int, int, string, string);
+        void inputWithdrawalDeposit(void);
+        void showBalance(void);
+        void interestCalc (void);
+        Logger log;
+};
 
-	  } else if (typeDeposit =='g'){
+void balanceCheck::interestCalc(void) {
+    if (typeDeposit == "s") {
+        interestSavingAcc = 0.25;
+    }
+    else if (typeDeposit =="g") {
+        if (accountBalance < 50000) {
+            interestGoldenAcc = 0.375;
+        }
+        else if (accountBalance > 50000) {
+		  interestGoldenAcc = 0.5;
+        }
+    }
+    else if (typeDeposit =="f") {
+        if (yearsFixedDeposit == 1) { // haven't have a way to assign the yearsFixedDeposit yet, overlap with ajay's work
+            interestFixedDeposit = 0.55;
+        }
+        else if (yearsFixedDeposit == 2){
+            interestFixedDeposit = 0.7;
+        }
+        else if (yearsFixedDeposit >= 3){
+            interestFixedDeposit = 0.8;
+        }
+    }
+}
 
-		  if (accountBalance < 50000){
-		  while (accountBalance <100){
-			  cout <<"\nThe minimum balance is $100, please make some deposit!"<<endl;
-			  inputWithdrawalDeposit();
-			  updateBalance();
-			  showBalance();
-		  }
-		  interestGoldenAcc = 0.375; 
-	  }
+void balanceCheck::inputWithdrawalDeposit(void) {
+    cout<<"Balance: $"<<accountBalance<<endl;
+    cout<<endl<<"Please select the mode - [W]ithdrawal or [D]eposit: ";
+    cin>>modeBalance;
+    while (modeBalance != "w" && modeBalance != "W" && modeBalance != "d" && modeBalance != "D"){
+        clearScreen();
+        showBankAccount();
+        cout<<"Balance: $"<<accountBalance<<endl;
+        cout<<"\nInvalid option! Please reselect:\n\n";
+        cout<<endl<<"Please select the mode - [W]ithdrawal or [D]eposit: ";
+        cin>>modeBalance;
+    }
 
-	  else if (accountBalance > 50000){
-		  interestGoldenAcc = 0.5; 
-		  
-	  } 
+    if (modeBalance == "D") modeBalance = "d";
+    else if(modeBalance == "W") modeBalance = "w";
 
-	  } else if (typeDeposit =='f'){
-		  while (accountBalance < 5000){
-		  cout <<"\nThe minimum balance is $5000, please make some deposit!"<<endl;
-		  inputWithdrawalDeposit();
-		  updateBalance();
-		  showBalance();
-	  }
-
-	  if (yearsFixedDeposit == 1){ // haven't have a way to assign the yearsFixedDeposit yet, overlap with ajay's work 
-		  interestFixedDeposit = 0.55;
-	  } else if (yearsFixedDeposit == 2){
-		  interestFixedDeposit = 0.7;
-	  } else if (yearsFixedDeposit >= 3){
-		  interestFixedDeposit = 0.8;
-	  }
-	  }
-
-  }
-  
-
-  void balanceCheck::inputWithdrawalDeposit(void){
-    cout <<"Please select the mode (Withdrawal : w / Deposit : d): ";
-    cin >> modeBalance; 
-
-	if (accountBalance <= 0 && modeBalance == 'w'){
-		cout <<endl <<"Gentle reminder: " << endl; 
-		cout <<"Your balance is lesser or equals to zero, unable to perform any withdrawal operations. " << endl;
-		return ; 
+	if (accountBalance <= 0 && modeBalance == "w") {
+        clearScreen();
+		cout<<endl<<"!!! Warning !!!"<<endl<<endl;
+		cout<<"Insufficient funds in account."<<endl;
+		inputWithdrawalDeposit();
 	}
-    
-    if (modeBalance == 'w'){
-      cout <<"Please enter the amount you wish to withdraw: " << endl;
-      cin >> withdrawalAmount;
-	  if (accountBalance < withdrawalAmount){
-		  cout <<"You do not have enough money!"<<endl;
-		  withdrawalAmount = 0;
-	  }
 
-    } else if (modeBalance == 'd'){
-      cout <<"Please enter the amount you wish to deposit: " << endl;
-      cin >> depositAmount;
-	  if (depositAmount < 0){
-		  cout <<"Invalid deposit amount" <<endl;
-		  depositAmount =0;
-	  }
+    else if (modeBalance == "w") {
+        cout<<"Please enter the amount you wish to withdraw: $";
+        cin>>withdrawalAmount;
+        while(!cin || !(withdrawalAmount > 0)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            clearScreen();
+            showBankAccount();
+            cout<<"Balance: $"<<accountBalance<<endl;
+            cout<<endl<<"Please select the mode - [W]ithdrawal or [D]eposit: "<<modeBalance<<"\n\n";
+            cout<<"Invalid amount! Please reenter:\n\n";
+            cout<<"Please enter the amount you wish to withdraw: $";
+            cin>>withdrawalAmount;
+        }
+        if (accountBalance < withdrawalAmount) {
+            clearScreen();
+            showBankAccount();
+            cout<<"\n\nYou cannot withdraw more money than available in your account. Operation terminating.\n"<<endl;
+            withdrawalAmount = 0;
+            inputWithdrawalDeposit();
+        }
+        else {
+            if (typeDeposit == "s") {
+                if ((accountBalance - withdrawalAmount) <= 100) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nSaving Accounts require a minimum of $100 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance - withdrawalAmount;
+                    log.withdrawDeposit(name, "withdrawn", withdrawalAmount, accountBalance);
+                }
+            }
+            else if (typeDeposit == "g") {
+                if ((accountBalance - withdrawalAmount) <= 100) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nGolden Accounts require a minimum of $100 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance - withdrawalAmount;
+                    log.withdrawDeposit(name, "withdrawn", withdrawalAmount, accountBalance);
+                }
+            }
+            else if (typeDeposit == "f") {
+                if ((accountBalance - withdrawalAmount) <= 5000) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nFixed Deposits require a minimum of $5,000 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance - withdrawalAmount;
+                    log.withdrawDeposit(name, "withdrawn", withdrawalAmount, accountBalance);
+                }
+            }
+        }
     }
-  }
-  
-  void balanceCheck::updateBalance (void){
-    if (modeBalance == 'd'){
-      accountBalance = accountBalance + depositAmount; 
-    } else if (modeBalance =='w'){
-      accountBalance = accountBalance - withdrawalAmount; 
+
+    else if (modeBalance == "d") {
+        cout<<"Please enter the amount you wish to deposit: $";
+        cin>>depositAmount;
+        while(!cin || !(depositAmount > 0)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            clearScreen();
+            showBankAccount();
+            cout<<"Balance: $"<<accountBalance<<endl;
+            cout<<endl<<"Please select the mode - [W]ithdrawal or [D]eposit: "<<modeBalance<<"\n\n";
+            cout<<"Invalid amount! Please reenter:\n\n";
+            cout<<"Please enter the amount you wish to deposit: $";
+            cin>>depositAmount;
+        }
+        if (depositAmount < 0) {
+            clearScreen();
+            showBankAccount();
+            cout<<"\n\nInvalid deposit amount. Operation terminating.\n"<<endl;
+            depositAmount = 0;
+            inputWithdrawalDeposit();
+        }
+        else {
+            if (typeDeposit == "s") {
+                if ((accountBalance + depositAmount) < 100) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nSaving Accounts require a minimum of $100 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance + depositAmount;
+                    log.withdrawDeposit(name, "deposited", depositAmount, accountBalance);
+                }
+            }
+            else if (typeDeposit == "g") {
+                if ((accountBalance + depositAmount) < 100) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nGolden Accounts require a minimum of $100 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance + depositAmount;
+                    log.withdrawDeposit(name, "deposited", depositAmount, accountBalance);
+                }
+            }
+            else if (typeDeposit == "f") {
+                if ((accountBalance + depositAmount) < 5000) {
+                    clearScreen();
+                    showBankAccount();
+                    cout<<"\nFixed Deposits require a minimum of $5,000 balance at all times.\n";
+                    inputWithdrawalDeposit();
+                }
+                else {
+                    accountBalance = accountBalance + depositAmount;
+                    log.withdrawDeposit(name, "deposited", depositAmount, accountBalance);
+                }
+            }
+        }
     }
-  }
-  
-  void balanceCheck:: showBalance (void) {
-    cout <<"Your balance is: " << accountBalance << endl << endl;
-  }
+}
 
+void balanceCheck::showBalance(void) {
+    cout<<"\n\nYour account balance is: $"<<accountBalance<<endl<<endl;
+}
 
- balanceCheck :: balanceCheck (string n, int acc, int a, char d, char g) : bankAccount(n,acc, a, d, g) {
+balanceCheck::balanceCheck(string n, int acc, int a, string d, string g):bankAccount(n, acc, a, d, g) {
 	name = n;
     accountNumber = acc;
     age = a;
     typeDeposit = d;
     gender = g;
-	depositAmount =0;
+	depositAmount = 0;
 	withdrawalAmount = 0;
 	accountBalance = 0;
-  }
+}
+
